5373baec8c482402a9b3c26eed525b2d757bc474
Jeetendra-Choudahry <mailme@jeetendra.xyz>
2017-02-06 09:06:50 +0530

    Initial Commit

---
+++ UIBtnProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIBtnProject
+//
+//  Created by Jeetendra Choudhary on 06/02/17.
+//  Copyright © 2017 xyz.jeetendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
