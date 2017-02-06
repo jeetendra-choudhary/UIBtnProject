b2e9a2c17f00ad96f947aeaed2ee3284833087d3
Jeetendra-Choudahry <mailme@jeetendra.xyz>
2017-02-06 09:18:33 +0530

    Initial Commit

---
+++ UIBtnProject/CustomBtn.swift
@@ -0,0 +1,24 @@
+//
+//  CustomBtn.swift
+//  UIBtnProject
+//
+//  Created by Jeetendra Choudhary on 06/02/17.
+//  Copyright © 2017 xyz.jeetendra. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func draw(_ rect: CGRect) {
+        
+    }
+}
--- UIBtnProject/ViewController.swift
+++ UIBtnProject/ViewController.swift
@@ -8,17 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIButton{
+    func roundedButton(){
+        let maskPAth1 = UIBezierPath(roundedRect: self.bounds,
+                                     byRoundingCorners: [.topLeft , .bottomRight],
+                                     cornerRadii:CGSize(width:20.0, height:20.0))
+        let maskLayer1 = CAShapeLayer()
+        maskLayer1.frame = self.bounds
+        maskLayer1.path = maskPAth1.cgPath
+        self.layer.mask = maskLayer1
+        
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var btn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        btn.roundedButton()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
